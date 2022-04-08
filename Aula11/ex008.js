7f15f741b0df8faf5be1c72b79b1f069a72b58ea
issacarbrun <issacarbrunow@gmail.com>
2022-04-08 16:06:56 -0300

    Mais aulas

---
+++ Aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 60;
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if(vel > 60)
+{
+    console.log(`MULTADO! Velocidade da via = 60 Km/h. Sua velocidade é ${vel}`)
+}
+else{
+    console.log('De boas')
+}
