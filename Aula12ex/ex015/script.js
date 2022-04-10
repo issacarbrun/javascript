74f66278d3740669a06a4b4d2570d3698ab93fb9
issacarbrun <issacarbrunow@gmail.com>
2022-04-10 09:26:51 -0300

    Exercicio Verificador de idade

---
+++ Aula12ex/ex015/script.js
@@ -0,0 +1,52 @@
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = document.querySelector('input#txtano');
+    var res = document.querySelector('div#res');
+
+    if(fano.value.length == 0 || fano.value >  ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else{
+        var fsex = document.getElementsByName('radsex');
+        var idade = ano - Number(fano.value);
+        var genero = '';
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if(fsex[0].checked) {
+            genero = 'Homem';
+            if(idade >= 0 && idade <10){
+                //Criança
+                img.setAttribute('src', 'foto_bebe_m.png')
+            } else if(idade < 21) {
+                //jovem
+                img.setAttribute('src', 'foto_jovem_m.png')
+            } else if(idade < 50) {
+                //adulto
+                img.setAttribute('src', 'foto_adulto_m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'foto_idoso_m.png')
+            }
+            
+        } else if (fsex[1].checked) {
+            genero = 'Mulher';
+            if(idade >=0 && idade <10) {
+                //Criança
+                img.setAttribute('src', 'foto_bebe_f.png')
+            } else if(idade < 21) {
+                //jovem
+                img.setAttribute('src', 'foto_jovem_f.png')
+            } else if(idade <50) {
+                //adulto
+                img.setAttribute('src', 'foto_adulto_f.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'foto_idoso_f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
